6ddb45a3408aa4cc93a90d7efce9a593329f8871
EricHoffmann57 <81462264+EricHoffmann57@users.noreply.github.com>
2022-03-17 22:58:15 +0100

    minor update

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -10,13 +10,13 @@ const Footer: React.FC = () => {
             <footer className={`footer red`}>
                 <div className={`innerFooter`}>
                     <div className="nameText">
-                        <a className="customLink hoverLink" href="htthttps://github.com/EricHoffmann57/react-netflix-clone-course.git" title="react-netflix-clone-course"><i className="fab fa-github"></i> | react-netflix-clone-course</a>
+                        <a className="customLink hoverLink" href="htthttps://github.com/EricHoffmann57/react-netflix-clone-course.git" title="react-netflix-clone-course"><i className="fab fa-github"></i></a>
                     </div>
-                    <a className="customLink hoverLink" href="https://fr.linkedin.com/" title="linkedIn logo"><i className="fab fa-linkedin"></i> | LinkedIn</a>
-                    <a className="customLink hoverLink" href="https://twitter.com/?lang=fr" title="twitter logo"><i className="fab fa-twitter"></i> | Twitter</a>
-                    <a className="customLink hoverLink" href="https://www.instagram.com/?hl=fr" title="instagram logo"><i className="fab fa-instagram"></i> | Instagram</a>
-                    <div className="siteText copyright" title="Copyright">npm
-                        <a href="./" className="hoverLink">Copyright <i className="fas fa-copyright"></i> {year}</a>
+                    <a className="customLink hoverLink" href="https://fr.linkedin.com/" title="linkedIn logo"><i className="fab fa-linkedin"></i> </a>
+                    <a className="customLink hoverLink" href="https://twitter.com/?lang=fr" title="twitter logo"><i className="fab fa-twitter"></i></a>
+                    <a className="customLink hoverLink" href="https://www.instagram.com/?hl=fr" title="instagram logo"><i className="fab fa-instagram"></i></a>
+                    <div className="siteText copyright" title="Copyright">
+                        <a href="./" className="hoverLink"><i className="fas fa-copyright"></i> {year}</a>
                     </div>
                 </div>
             </footer>
